191b1d2dca524775d112bdfaa5d05f2242218e78
Amondt <mondt.alexande>
2019-01-28 16:07:01 +0100

    Exe 8

---
+++ js_function.js
@@ -0,0 +1,12 @@
+function resteDiv (a, b) {
+    alert("Le reste de leur division vaut " + (a%b));
+}
+function calculResult (pointure, naissance) {
+    pointure *= 2;
+    pointure += 5;
+    pointure *= 50;
+    pointure -= naissance;
+    pointure += 1766;
+
+    return pointure;
+}
