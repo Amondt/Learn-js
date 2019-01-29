b9fb911c25eb6b281799587528870ca29793c531
Amondt <mondt.alexande>
2019-01-29 22:41:13 +0100

    ex03 done

--- js/js_function.js
+++ js/js_function.js
@@ -1,9 +1,9 @@
 function resteDiv (a, b) {
     alert("Le reste de leur division vaut " + (a%b));
 }
-function calculResult (e) {
+function calculResult(e){
     e.preventDefault();
-    var form = document.querySelector('#pointure');
+    // var form = document.querySelector('#pointure');
     var pointure = e.target.pointure.value;
     var naissance = e.target.annee.value;
     console.log(e.target.pointure.value);
@@ -12,6 +12,7 @@ function calculResult (e) {
     pointure *= 50;
     pointure -= naissance;
     pointure += 1766;
+    alert("voir console.log");
     console.log("le resultat est ", pointure);
     return pointure;
 }
@@ -31,4 +32,29 @@ function testAge(e){
     else if (form.age.value < 18){
         alert("Vous êtes mineur");
     }
+}
+function surfaceRectangle(e){
+    e.preventDefault();
+    var largeur = prompt('largeur');
+    var hauteur = prompt('hauteur');
+    alert('La surface de ce rectangle vaut : ' + (hauteur*largeur));
+}
+function periRectangle(e){
+    e.preventDefault();
+    var largeur = prompt('largeur');
+    var hauteur = prompt('hauteur');
+    alert('Le périmètre de ce rectangle vaut : ' + (2*hauteur + 2*largeur));
+}
+function periCircle(e){
+    e.preventDefault();
+    var rayon = prompt('Rayon');
+    alert('La surface de ce cercle vaut : ' + (Math.PI * Math.pow(rayon, 2)).toFixed(2));
+}
+function triple(x){
+    return 3*x;
+}
+function affiche(e){
+    e.preventDefault();
+    alert(triple(a));
+    alert(triple(b));
 }
