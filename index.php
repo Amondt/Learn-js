87db27cb3ed1cb6c2e618db7ca420916b0460bb7
Amondt <mondt.alexande>
2019-02-20 11:07:57 +0100

    index.php add

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-    if (isset($_POST['pseudo']) && isset($_POST['mdp']) && isset($_POST['courriel'])) {
+    if (isset($_POST['name']) && isset($_POST['mdp']) && isset($_POST['courriel'])) {
 
-        $pseudo = $_POST['pseudo'];
+        $pseudo = $_POST['name'];
         $mdp = $_POST['mdp'];
         $courriel = $_POST['courriel'];
 
@@ -9,8 +9,8 @@
         $subject = 'test form | email send';
         $body = 'bla bla bla';
 
-        $headers  = 'Form: '.$pseudo.' <'.$email.'>\r\n';
-        $headers .= 'Reply-To: '.$email.'\r\n';
+        $headers  = 'Form: '.$name.' <'.$courriel.'>\r\n';
+        $headers .= 'Reply-To: '.$courriel.'\r\n';
         $headers .= 'MIME-Version: 1.0\r\n';
         $headers .= 'Content-type: text/html; charset=utf-8';
 
@@ -33,43 +33,49 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Sleephere booking</title>
-        <link rel="stylesheet" href="./css/style.css">
-        <script src="./js/form-sleephere.js"></script>
     </head>
     <body>
-        <h1>Formulaire d'inscription</h1>
-        <form action='' method='POST'>
+        <h1>Contact Us</h1>
+        <p>You want to book ? You got a question ? We'd love to hear from you. Send us a message ans we'll respond as soon as possible.</p>
+        <form method='POST'>
             <p>
-                <label for="pseudo">Pseudo</label> :
-                <input type="text" name="pseudo" id="pseudo" required>
-                <span id="aidePseudo"></span>
+                <label for="name">Name*</label>
+                <input type="text" name="name" id="name" required>
+                <span id="aideName"></span>
             </p>
             <p>
-                <label for="mdp">Mot de passe</label> :
+                <label for="courriel">Email*</label>
+                <input type="email" name="courriel" id="courriel" required placeholder="example@domain.com">
+                <span id="aideCourriel"></span>
+            </p>
+            <p>
+                <label for="mdp">Message</label>
                 <input type="password" name="mdp" id="mdp" required>
                 <span id="aideMdp"></span>
             </p>
             <p>
-                <label for="courriel">Courriel</label> :
-                <input type="email" name="courriel" id="courriel" required placeholder="utilisateur@domaine.fr">
-                <span id="aideCourriel"></span>
-            </p>
-            <p>
-                <input type="submit" value="Envoyer">
-                <input type="reset" value="Annuler">
+                <input type="submit" value="Send Message">
             </p>
 
             <script>
                 window.onload = () => {
-                    // Pseudo
-                    var pseudoElt = document.querySelector('#pseudo')
-                    document.querySelector('#pseudo').focus()
-                    // pseudoElt.value = 'pseudo'
-                    pseudoElt.addEventListener('focus', function(e) {
-                        document.querySelector('#aidePseudo').textContent = 'Entrez votre pseudo'
+                    // Name
+                    var nameElt = document.querySelector('#name')
+                    nameElt.addEventListener('mouseover', function(e) {
+                        document.querySelector('#aideName').textContent = 'Enter your name'
                     })
-                    pseudoElt.addEventListener('blur', function(e) {
-                        document.querySelector('#aidePseudo').textContent = ''
+                    nameElt.addEventListener('mouseout', function(e) {
+                        document.querySelector('#aideName').textContent = ''
+                    })
+
+                    // Courriel
+                    document.querySelector('#courriel').addEventListener('blur', function(e) {
+                    var regexCourriel = /.+@.+\..+/
+                    var validiteCourriel = ''
+                    if (!regexCourriel.test(e.target.value) && (e.target.value != '')) {
+                        validiteCourriel = 'invalid address'
+                    }
+                    document.querySelector('#aideCourriel').textContent = validiteCourriel
                     })
 
                     // Mdp
@@ -87,16 +93,6 @@
                         var aideMdpElt = document.querySelector("#aideMdp");
                         aideMdpElt.textContent = "Longueur : " + longueurMdp; // Texte de l'aide
                         aideMdpElt.style.color = couleurMsg; // Couleur du texte de l'aide
-                    })
-
-                    // Courriel
-                    document.querySelector('#courriel').addEventListener('blur', function(e) {
-                    var regexCourriel = /.+@.+\..+/
-                    var validiteCourriel = ''
-                    if (!regexCourriel.test(e.target.value)) {
-                        validiteCourriel = 'adresse invalide'
-                    }
-                    document.querySelector('#aideCourriel').textContent = validiteCourriel
                     })
                 }
             </script>
